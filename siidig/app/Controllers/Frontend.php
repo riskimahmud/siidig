4bc14f242b88bf01817748ca69093ebd4584175a
riskimahmud <riskyantomahmud@gmail.com>
2022-03-20 22:11:59 +0800

    update list pelatihan on front

--- siidig/app/Controllers/Frontend.php
+++ siidig/app/Controllers/Frontend.php
@@ -17,7 +17,7 @@ class Frontend extends BaseController
     {
         $data = [
             'header' => $this->crud_model->select_data('header', 'getResultArray', ['status' => 'publish']),
-            'pelatihan' => $this->crud_model->select_data('pelatihan', 'getResultArray', ['status' => 'publish', 'tgl_pelaksanaan <=' => date("Y-m-d")], null, ['tgl_pelaksanaan' => 'DESC'], 3),
+            'pelatihan' => $this->crud_model->select_data('pelatihan', 'getResultArray', ['status' => 'publish', 'tgl_pelaksanaan >=' => date("Y-m-d")], null, ['tgl_pelaksanaan' => 'DESC'], 3),
             'berita' => $this->crud_model->select_data('berita', 'getResultArray', ['status' => 'publish'], null, ['created_at' => 'DESC'], 3),
             'aplikasi' => $this->crud_model->select_data('aplikasi', 'getResultArray'),
             'count' => $this->crud_model->select_data('grafik_tahunan', 'getRow', false, null, ['tahun' => 'DESC'])
