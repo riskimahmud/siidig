c3e0cc311ad96f0967b5113be12e477035759b1c
riskimahmud <riskyantomahmud@gmail.com>
2022-03-22 11:17:26 +0800

    add if empty data

--- siidig/app/Views/frontend/berita.php
+++ siidig/app/Views/frontend/berita.php
@@ -23,29 +23,39 @@
 <section class="blog-page pad-tb pt40">
     <div class="container">
 
-        <div class="row">
 
-            <?php foreach ($data as $d) : ?>
-                <div class="col-lg-4 mt60">
-                    <div class="single-blog-post- shdo">
-                        <div class="single-blog-img-">
-                            <a href="<?= base_url("/berita/" . $d->slug); ?>"><img src="<?= base_url("uploads/berita/thumb_" . $d->gambar); ?>" alt="girl" class="img-fluid"></a>
-                            <div class="entry-blog-post dg-bg2">
-                                <span class="bypost-"><a href="javascript:void(0)"><i class="fas fa-fw fa-user-edit"></i> <?= $d->penulis ?></a></span>
-                                <span class="posted-on-">
-                                    <a href="javascript:void(0)"><i class="fas fa-fw fa-clock"></i> <?= tgl_indonesia($d->created_at) ?></a>
-                                </span>
+        <div class="row">
+            <?php if (!empty($data)) : ?>
+
+                <?php foreach ($data as $d) : ?>
+                    <div class="col-lg-4 mt60">
+                        <div class="single-blog-post- shdo">
+                            <div class="single-blog-img-">
+                                <a href="<?= base_url("/berita/" . $d->slug); ?>"><img src="<?= base_url("uploads/berita/thumb_" . $d->gambar); ?>" alt="girl" class="img-fluid"></a>
+                                <div class="entry-blog-post dg-bg2">
+                                    <span class="bypost-"><a href="javascript:void(0)"><i class="fas fa-fw fa-user-edit"></i> <?= $d->penulis ?></a></span>
+                                    <span class="posted-on-">
+                                        <a href="javascript:void(0)"><i class="fas fa-fw fa-clock"></i> <?= tgl_indonesia($d->created_at) ?></a>
+                                    </span>
+                                </div>
                             </div>
-                        </div>
-                        <div class="blog-content-tt">
-                            <div class="single-blog-info-">
-                                <h4><a href="<?= base_url("/berita/" . $d->slug); ?>"><?= $d->title; ?></a></h4>
-                                <p><?= $d->excerpt; ?></p>
+                            <div class="blog-content-tt">
+                                <div class="single-blog-info-">
+                                    <h4><a href="<?= base_url("/berita/" . $d->slug); ?>"><?= $d->title; ?></a></h4>
+                                    <p><?= $d->excerpt; ?></p>
+                                </div>
                             </div>
                         </div>
                     </div>
+                <?php endforeach; ?>
+
+            <?php else : ?>
+                <div class="col-lg-12">
+                    <div class="bg-gradient pt60 pb60 mt30 mb30 text-center">
+                        <h4><i class="fas fa-meh"></i> Maaf. Belum ada berita.</h4>
+                    </div>
                 </div>
-            <?php endforeach; ?>
+            <?php endif; ?>
         </div>
     </div>
 </section>
--- siidig/app/Views/frontend/pelatihan.php
+++ siidig/app/Views/frontend/pelatihan.php
@@ -23,27 +23,35 @@
 <section class="blog-page pad-tb pt40">
     <div class="container">
         <div class="row">
-            <?php foreach ($data as $d) : ?>
-                <div class="col-lg-4 col-sm-6 single-card-item">
-                    <div class="isotope_item hover-scale">
-                        <div class="item-image">
-                            <a href="<?= base_url("course/" . $d->slug); ?>"><img src="<?= base_url("uploads/pelatihan/thumb_" . $d->gambar); ?>" alt="pelatihan" class="img-fluid" /> </a>
-                            <!-- <span class="category-blog"><a href="#">iOs</a> <a href="#">Android</a></span> -->
-                        </div>
-                        <div class="item-info blog-info text-center">
-                            <div class="entry-blog">
-                                <span class="bypost"><i class="fas fa-map-marker"></i> <?= $d->lokasi; ?></span>
-                                <span class="posted-on">
-                                    <i class="fas fa-calendar-alt"></i> <?= tgl_indonesia($d->tgl_pelaksanaan) ?>
-                                </span>
-                                <!-- <span><a href="#"><i class="fas fa-comment-dots"></i> (23)</a></span> -->
+            <?php if (!empty($data)) : ?>
+                <?php foreach ($data as $d) : ?>
+                    <div class="col-lg-4 col-sm-6 single-card-item">
+                        <div class="isotope_item hover-scale">
+                            <div class="item-image">
+                                <a href="<?= base_url("course/" . $d->slug); ?>"><img src="<?= base_url("uploads/pelatihan/thumb_" . $d->gambar); ?>" alt="pelatihan" class="img-fluid" /> </a>
+                                <!-- <span class="category-blog"><a href="#">iOs</a> <a href="#">Android</a></span> -->
                             </div>
-                            <h4><a href="<?= base_url("course/" . $d->slug); ?>"><?= $d->title ?></a></h4>
-                            <!-- <p></p> -->
+                            <div class="item-info blog-info text-center">
+                                <div class="entry-blog">
+                                    <span class="bypost"><i class="fas fa-map-marker"></i> <?= $d->lokasi; ?></span>
+                                    <span class="posted-on">
+                                        <i class="fas fa-calendar-alt"></i> <?= tgl_indonesia($d->tgl_pelaksanaan) ?>
+                                    </span>
+                                    <!-- <span><a href="#"><i class="fas fa-comment-dots"></i> (23)</a></span> -->
+                                </div>
+                                <h4><a href="<?= base_url("course/" . $d->slug); ?>"><?= $d->title ?></a></h4>
+                                <!-- <p></p> -->
+                            </div>
                         </div>
                     </div>
+                <?php endforeach; ?>
+            <?php else : ?>
+                <div class="col-lg-12">
+                    <div class="bg-gradient pt60 pb60 mt30 mb30 text-center">
+                        <h4><i class="fas fa-meh"></i> Maaf. Belum ada pelatihan.</h4>
+                    </div>
                 </div>
-            <?php endforeach; ?>
+            <?php endif; ?>
         </div>
     </div>
 </section>
